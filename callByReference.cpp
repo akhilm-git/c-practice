c001b24db2cd9609bb6bc564d202d830bd98d6d5
Akhil Mishra <83783709+akhilm-git@users.noreply.github.com>
2024-09-16 13:41:51 +0530

    Add files via upload

---
+++ callByReference.cpp
@@ -0,0 +1,32 @@
+/******************************************************************************
+
+Welcome to GDB Online.
+GDB online is an online compiler and debugger tool for C, C++, Python, Java, PHP, Ruby, Perl,
+C#, OCaml, VB, Swift, Pascal, Fortran, Haskell, Objective-C, Assembly, HTML, CSS, JS, SQLite, Prolog.
+Code, Compile, Run and Debug online from anywhere in world.
+
+*******************************************************************************/
+//pass by reference
+
+#include <iostream>
+
+
+void swap(int &x,int &y){
+    int temp=0;
+    temp=x;
+    x=y;
+    y=temp;
+}
+
+
+int main()
+{
+    int a,b;
+    
+    std::cout<<"Hello World"<<std::endl;
+    std::cin >> a;
+    std::cin >>b;
+    swap(a,b);
+    std::cout<<a <<" "<<b;
+    
+}
